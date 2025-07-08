40e6c0d649a6a437c7a5ca7c1c0506b8b76f5de8
Izzi Digital <izzigallery2021@gmail.com>
2025-07-08 22:08:50 +0700

    Mengubah ke bahasa indonesia

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -127,27 +127,21 @@
           <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
           </div>
           <div class="mx-auto max-w-2xl text-center">
-            <h2 class="text-4xl font-semibold tracking-tight text-balance text-gray-900 sm:text-5xl">Contact sales</h2>
-            <p class="mt-2 text-lg/8 text-gray-600">Aute magna irure deserunt veniam aliqua magna enim voluptate.</p>
+            <h2 class="text-4xl font-semibold tracking-tight text-balance text-gray-900 sm:text-5xl">Kontak Kami</h2>
+            <p class="mt-2 text-lg/8 text-gray-600">hubungi kami terkait kerjasama atau feedback apapun.</p>
           </div>
           <form action="#" method="POST" class="mx-auto mt-16 max-w-xl sm:mt-20">
             <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
               <div>
-                <label for="first-name" class="block text-sm/6 font-semibold text-gray-900">First name</label>
+                <label for="first-name" class="block text-sm/6 font-semibold text-gray-900">Nama Depan</label>
                 <div class="mt-2.5">
                   <input type="text" name="first-name" id="first-name" autocomplete="given-name" class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600" />
                 </div>
               </div>
               <div>
-                <label for="last-name" class="block text-sm/6 font-semibold text-gray-900">Last name</label>
+                <label for="last-name" class="block text-sm/6 font-semibold text-gray-900">Nama Belakang</label>
                 <div class="mt-2.5">
                   <input type="text" name="last-name" id="last-name" autocomplete="family-name" class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600" />
-                </div>
-              </div>
-              <div class="sm:col-span-2">
-                <label for="company" class="block text-sm/6 font-semibold text-gray-900">Company</label>
-                <div class="mt-2.5">
-                  <input type="text" name="company" id="company" autocomplete="organization" class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600" />
                 </div>
               </div>
               <div class="sm:col-span-2">
@@ -157,25 +151,23 @@
                 </div>
               </div>
               <div class="sm:col-span-2">
-                <label for="phone-number" class="block text-sm/6 font-semibold text-gray-900">Phone number</label>
+                <label for="phone-number" class="block text-sm/6 font-semibold text-gray-900">Nomor HP</label>
                 <div class="mt-2.5">
                   <div class="flex rounded-md bg-white outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600">
                     <div class="grid shrink-0 grid-cols-1 focus-within:relative">
                       <select id="country" name="country" autocomplete="country" aria-label="Country" class="col-start-1 row-start-1 w-full appearance-none rounded-md py-2 pr-7 pl-3.5 text-base text-gray-500 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
-                        <option>US</option>
-                        <option>CA</option>
-                        <option>EU</option>
+                        <option>+62</option>
                       </select>
                       <svg class="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4" viewBox="0 0 16 16" fill="currentColor" aria-hidden="true" data-slot="icon">
                         <path fill-rule="evenodd" d="M4.22 6.22a.75.75 0 0 1 1.06 0L8 8.94l2.72-2.72a.75.75 0 1 1 1.06 1.06l-3.25 3.25a.75.75 0 0 1-1.06 0L4.22 7.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
                       </svg>
                     </div>
-                    <input type="text" name="phone-number" id="phone-number" class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" placeholder="123-456-7890" />
+                    <input type="text" name="phone-number" id="phone-number" class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" placeholder="8xx-xxxx-xxxx" />
                   </div>
                 </div>
               </div>
               <div class="sm:col-span-2">
-                <label for="message" class="block text-sm/6 font-semibold text-gray-900">Message</label>
+                <label for="message" class="block text-sm/6 font-semibold text-gray-900">Pesan Singkat</label>
                 <div class="mt-2.5">
                   <textarea name="message" id="message" rows="4" class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"></textarea>
                 </div>
@@ -188,13 +180,13 @@
                   </div>
                 </div>
                 <label class="text-sm/6 text-gray-600" for="agree-to-policies">
-                  By selecting this, you agree to our
-                  <a href="#" class="font-semibold whitespace-nowrap text-indigo-600">privacy policy</a>.
+                  Dengan Klik, anda setuju dengan peraturan kami
+                  <a href="#" class="font-semibold whitespace-nowrap text-indigo-600">peraturan</a>.
                 </label>
               </div>
             </div>
             <div class="mt-10">
-              <button type="submit" class="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Let's talk</button>
+              <button type="submit" class="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Yuk, Berbincang</button>
             </div>
           </form>
         </div>
