aa27dd396fe917c15007507e49e3f5394ffa4c3b
Izzi Digital <izzigallery2021@gmail.com>
2025-07-12 11:27:20 +0700

    menambah title untuk halaman nya

--- routes/web.php
+++ routes/web.php
@@ -3,21 +3,32 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome', [
+        'title' => 'Home'
+    ]);
 });
 
 Route::get('/contact', function () {
-    return view('contact', ['nama' => 'Ahmad Naqi Humam']);
+    return view('contact', [
+        'nama' => 'Ahmad Naqi Humam',
+        'title' => 'Contact'
+    ]);
 });
 
 Route::get('/blog', function () {
-    return view('blog');
+    return view('blog', [
+        'title' => 'Blog'
+    ]);
 });
 
 Route::get('/home', function () {
-    return view('welcome');
+    return view('welcome', [
+        'title' => 'Home'
+    ]);
 });
 
 Route::get('/about', function () {
-    return view('about');
+    return view('about', [
+        'title' => 'About'
+    ]);
 });
